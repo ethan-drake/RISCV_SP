396e0ccceee00b2586d925edb16b297774284c54
Eduardo Ethandrake Castillo Pulido <eduardo_ethandrake@yahoo.com.mx>
2024-11-09 19:56:33 -0600

    test 10 asm code added

---
+++ asm/test_10.asm
@@ -0,0 +1,11 @@
+#TEST 10: More that one ready at the same time for the  issue unit
+addi t1, zero, 0x1e
+addi t2, zero, 0x3
+div a2,t1,t2
+addi t0, a2, 0x1e
+mul a1,a2,t2
+div a3, t1, t2
+addi s1, a2, 0x1a
+addi a0, a2, 0x4
+addi s7, a2, 0x4
+fin: jal zero, fin 
